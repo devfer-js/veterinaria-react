882fd98fab7f0c82cbdd2208eb101453d91b0849
ferkki <ferkki@hotmail.com>
2019-10-18 18:15:40 -0400

    Updates

---
+++ precache-manifest.a2393a7838593181da6e046f01c6b692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f135c4d3a1db9d8789aab2c1956bd94f",
+    "url": "/veterinaria-react/index.html"
+  },
+  {
+    "revision": "e27390e2d93024215f5f",
+    "url": "/veterinaria-react/static/css/main.4a0a90cb.chunk.css"
+  },
+  {
+    "revision": "1bdfabf0d50ddce3ed73",
+    "url": "/veterinaria-react/static/js/2.a7dfad6f.chunk.js"
+  },
+  {
+    "revision": "e27390e2d93024215f5f",
+    "url": "/veterinaria-react/static/js/main.ab937464.chunk.js"
+  },
+  {
+    "revision": "32c7adea36636b4135a0",
+    "url": "/veterinaria-react/static/js/runtime-main.e801a7e3.js"
+  }
+]);
